fd6794c8700563a9d9f893cde5b193b1ee962523
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2018-11-28 11:50:08 -0500

    functionizing

--- point-to-ward/point-to-ward.py
+++ point-to-ward/point-to-ward.py
@@ -1,17 +1,46 @@
 from fastkml import kml
 from shapely.geometry import Point, Polygon
+import pandas as pd
 
-file = open('wards.kml', 'r', encoding="utf-8")
-doc = file.read()
+def kml_to_wards():
+    """
+    Opens the .kml, parses it, and returns the list of wards.
+    """
 
-k = kml.KML()
-k.from_string(doc)
-features = list(k.features())
-place = list(features[0].features())
-wards = list(place[0].features())
+    # Open and encode
+    file = open('wards.kml', 'r', encoding="utf-8")
+    doc = file.read()
 
-toronto = Point(43.6532, -79.3832)
-print(toronto.within(wards[0].geometry))
+    # Read into KML
+    k = kml.KML()
+    k.from_string(doc)
+
+    # Traverse the KML to ward lists
+    features = list(k.features())
+    place = list(features[0].features())
+    wards = list(place[0].features())
+
+    return wards
 
 
+def which_ward(wards, lat, lng):
+    """
+    Takes the `wards` list, and the `lat`/`lng` of the terminal, outputs
+    the ward it belongs to.
+    """
+    
+    bike_terminal = Point(lng, lat) # Weird convention?
+
+    # This is gross, but do it anyway
+    for ward in wards:
+        if bike_terminal.within(ward.geometry):
+            print(ward.name)
+
+
+def read_terminals(filename):
+    pass
+
+
+wards = kml_to_wards()
+which_ward(wards, 43.6532, -79.3832)
 
