fafd17a688d35a6a55cd94965985c10001882d73
Aaron Rambhajan <aaron.rambhajan@mail.utoronto.ca>
2018-11-28 12:34:12 -0500

    it works now

--- point-to-ward/point-to-ward.py
+++ point-to-ward/point-to-ward.py
@@ -2,13 +2,20 @@ from fastkml import kml
 from shapely.geometry import Point, Polygon
 import pandas as pd
 
-def kml_to_wards():
+KML_FILE = 'wards.kml'
+TERMINAL_FILE = 'terminals.csv'
+OUTPUT_FILE = 'terminals_and_wards.csv'
+
+def initialize_wards(filename):
     """
-    Opens the .kml, parses it, and returns the list of wards.
-    """
+    Open the .kml, parses it, and returns the list of wards.
+
+    .kml:    https://www.google.com/maps/d/u/0/viewer?mid=1ZH7qeq9DUOy7C4KYBXvtn1EbiFrSkBSk&ll=43.65017152286006%2C-79.37985265290109&z=16
+    fastkml: https://fastkml.readthedocs.io/en/latest/reference_guide.html
+    """ 
 
     # Open and encode
-    file = open('wards.kml', 'r', encoding="utf-8")
+    file = open(filename, 'r', encoding="utf-8")
     doc = file.read()
 
     # Read into KML
@@ -27,20 +34,20 @@ def which_ward(wards, lat, lng):
     """
     Takes the `wards` list, and the `lat`/`lng` of the terminal, outputs
     the ward it belongs to.
+
+    https://automating-gis-processes.github.io/2017/lessons/L3/point-in-polygon.html
     """
-    
-    bike_terminal = Point(lng, lat) # Weird convention?
+
+    bike_terminal = Point(lng, lat)  # Weird convention?
 
     # This is gross, but do it anyway
     for ward in wards:
         if bike_terminal.within(ward.geometry):
-            print(ward.name)
+            return ward.name.split()[-1]  # Assumes it'll only fit into _one_ ward
 
 
-def read_terminals(filename):
-    pass
 
-
-wards = kml_to_wards()
-which_ward(wards, 43.6532, -79.3832)
-
+ward_list = initialize_wards(KML_FILE)
+df = pd.read_csv(TERMINAL_FILE)
+df['Ward'] = df.apply(lambda row: which_ward(ward_list, row['Latitude'], row['Longitude']), axis=1)
+df.to_csv(OUTPUT_FILE)
---
+++ run-point-to-ward.sh
@@ -0,0 +1,3 @@
+# chmod u+x run.sh
+cd point-to-ward/
+python3 point-to-ward.py
